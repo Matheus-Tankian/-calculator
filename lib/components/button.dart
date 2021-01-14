9e6ea7a3ab44cee5b5580507f971f2adcce1024a
Matheus <matheusvitor8143@gmail.com>
2021-01-13 22:44:59 -0300

    interface

---
+++ lib/components/button.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  static const DARK = Color.fromRGBO(82, 82, 82, 1);
+  static const DEFAULT = Color.fromRGBO(112, 112, 112, 1);
+  static const OPERATION = Color.fromRGBO(250, 158, 13, 1);
+
+  final Color color;
+  final String text;
+  final bool big;
+  final void Function(String) cb;
+
+  Button({
+   @required this.text,
+    this.big = false,
+    this.color = DEFAULT,
+    @required this.cb,
+  });
+
+  Button.big({
+    @required this.text,
+    this.big = true,
+    this.color = DEFAULT,
+    @required this.cb,
+  });
+
+  Button.operation({
+    @required this.text,
+    this.big = false,
+    this.color = OPERATION,
+    @required this.cb,
+  });
+
+  Button.dark({
+    @required this.text,
+    this.big = false,
+    this.color = DARK,
+    @required this.cb,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: big ? 2 : 1,
+      child: RaisedButton(
+        color: this.color,
+        child: Text(
+          text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 32.0,
+            fontWeight: FontWeight.w200,
+          ),
+        ),
+        onPressed: () => cb(text),
+      ),
+    );
+  }
+}
--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,15 +1,30 @@
+import 'package:calculadora/models/memory.dart';
 import 'package:flutter/material.dart';
 import '../components/display.dart';
+import '../components/keyboard.dart';
 
-class Calculator extends StatelessWidget {
+
+class Calculator extends StatefulWidget {
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  final Memory memory = Memory();
+
+  _onPressed(String command) {
+    setState(() {
+      memory.applyCommand(command);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Column(
         children: [
-          Display('123.45'),
-          Text('Keyboard'),
-
+          Display(memory.value),
+          Keyboard(_onPressed),
         ],
       ),
     );
